6549d35419355e82a27e6cdf5606e4f622e4c8f7
ThienNK <thiennk@ominext.com>
2023-05-15 13:36:39 +0700

    add logic signin catch error signup and add config library

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -13,28 +15,46 @@ export class AuthService {
 
     // save the new user to the database
     try {
+      const user = await this.prisma.user.create({
+        data: {
+          hash: hash,
+          email: dto.email,
+        },
+      });
 
-        const user = await this.prisma.user.create({
-            data: {
-                hash: hash,
-                email: dto.email,
-            },
-            select: {
-                id: true,
-                email: true,
-                createdAt: true,
-                updatedAt: true,
-            },
-        });
-        
-        // return the saved user
-        return user;
+      // return the saved user
+      delete user.hash;
+      return user;
     } catch (err) {
-        throw err;
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('this email should be unique');
+        }
+      }
+      throw err;
     }
   }
 
-  signin() {
-    return 'you are signed in';
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if the user doesnt exist, throw exception
+    if (!user) {
+      throw new ForbiddenException('User does not exist');
+    }
+    // compare the password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if the passwword incorrect, throw exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Password is incorrect');
+    }
+
+    // if the passwords match, return the user
+    delete user.hash;
+    return user;
   }
 }
