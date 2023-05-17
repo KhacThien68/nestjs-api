56fe1ababc01c21f2e2d973809aa06ba3ab5abbb
ThienNK <thiennk@ominext.com>
2023-05-17 10:48:51 +0700

    add logic and e2e testing edit user

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AuthDto } from '../src/auth/dto';
+import { EditUserDto } from '../src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -118,7 +119,22 @@ describe('App e2e', () => {
       });
     });
 
-    describe('Edit user', () => {});
+    describe('Edit user', () => {
+      it('Should eidt user', () => {
+        const dto: EditUserDto = {
+          firstName: 'John',
+          email: 'john@example.com',
+        };
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({
+            Authorization: `Bearer $S{userAt}`,
+          })
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
   });
 
   describe('Bookmarks', () => {
